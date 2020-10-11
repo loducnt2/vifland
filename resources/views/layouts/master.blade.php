1a5f13512ab0fc3976b12f928bef8b30b221152e
QuangNguyen1607 <55746340+QuangNguyen1607@users.noreply.github.com>
2020-10-11 21:05:19 +0700

    nguyen

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,6 +9,7 @@
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
 		<link rel="stylesheet" href="{{asset('css/core.min.css')}}">
 		<link rel="stylesheet" href="{{asset('css/main.min.css')}}">
+		<link rel="stylesheet" href="{{asset('css/update.css')}}">
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css">
 		<link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css">
 
