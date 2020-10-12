642c754143efa3147e3b255a0720941bb8dcc44b
buinguyenhoangtho1997 <buinguyenhoangtho1997@gmail.com>
2020-10-12 13:01:58 +0700

    Admin CSS hoàn chỉnh

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,9 +1,8 @@
 
 {{-- <link rel="stylesheet" href="{{asset('../css/admin-styles.css') }}"> --}}
     <link href="../css/admin_styles.css" rel="stylesheet" />
-<link rel="stylesheet" href="{{asset('css/styles.css') }}">
-    <link href="css/admin_styles.css" rel="stylesheet" />
-    <link href="css/update-admin.css" rel="stylesheet" />
+
+
 
     <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
