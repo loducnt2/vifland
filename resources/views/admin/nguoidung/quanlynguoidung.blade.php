5a85f113450650a50bfe845c75adf53dd119da43
hoangtho1997 <buinguyenhoangtho1997@gmail.com>
2020-10-20 22:58:24 +0700

    Fix bug

--- resources/views/admin/nguoidung/quanlynguoidung.blade.php
+++ resources/views/admin/nguoidung/quanlynguoidung.blade.php
@@ -52,7 +52,33 @@
                   </td>
 
                   <td>
-                    <a href="user-profile/{{$user->id}}">Link</a>
+                   {{-- modal box here --}}
+                   <!-- Button trigger modal -->
+<button type="button" class="btn btn-warning" data-toggle="modal" data-target="#exampleModal">
+    Khoá thành viên
+  </button>
+
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Khoá thành viên {{$profile->id}}</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
                   </td>
                  @endforeach
 
