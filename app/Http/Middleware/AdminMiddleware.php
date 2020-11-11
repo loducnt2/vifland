52edac202c718763e68606ad58ca88b4829dade5
Hoangtho1997 <buinguyenhoangtho1997@gmail.com>
2020-11-11 13:43:27 +0700

    Thọ - quyền admin

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -19,12 +19,12 @@ class AdminMiddleware
         // @if( && Auth::user()->user_type == "1")
 
         // nếu user type = 0, tức là user role thì sẽ bay về route login
-        if (Auth::check() && Auth::user()->user_type=="0") {
-            return redirect('/login');
+        if (Auth::check() && Auth::user()->user_type=="1") {
+            return $next($request);
         }
         else{
             return redirect('/');
         }
-        return $next($request);
+
     }
 }
