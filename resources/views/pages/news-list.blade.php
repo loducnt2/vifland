a9592b5fc8ef70b884ec3a76dbd4a230bf8f5956
Lâm Trần <lamzaaa@yahoo.com>
2020-11-23 16:21:14 +0700

    up

--- resources/views/pages/news-list.blade.php
+++ resources/views/pages/news-list.blade.php
@@ -2,7 +2,7 @@
 @section('title','Danh sách tin tức')
 @section('headerStyles')
 <!-- Thêm styles cho trang này ở đây-->
- @stop
+@stop
 @section('content')
 <main>
 	<section class="banner-top">
@@ -13,44 +13,53 @@
 			<ol class="breadcrumb">
 				<li class="breadcrumb-item"><a href="#"> <i class="ri-arrow-left-line icons-breadcrum"></i>Mua/ Bán <span class="sll-breadcrum">&nbsp; (1.475.822 tin đăng)</span></a></li>
 				<li class="breadcrumb-item"><a href="#">
-						<p>Mở bán dự án đô thị sinh thái thông minh Aqua City, phía Đông thành phố Hồ Chí Minh Bạn tìm gì hôm nay?</p></a></li>
+						<p>Mở bán dự án đô thị sinh thái thông minh Aqua City, phía Đông thành phố Hồ Chí Minh Bạn tìm gì hôm nay?</p>
+					</a></li>
 			</ol>
 		</div>
 	</div>
 	<section class="pages-news-list">
 		<div class="container">
 
-            <h1 class="section-title text-uppercase">tin tức mới nhất </h1>
+			<h1 class="section-title text-uppercase">tin tức mới nhất </h1>
 			<div class="row sec-1">
 				<div class="col-xl-6 col-md-6 col-12">
-                <div class="article-big"><a class="bg" href="/news/{{$latest[0]->slug}}"><img src="{{asset('assets/news/' .$latest[0]->img)}}" alt=""></a>
+					<div class="article-big"><a class="bg" href="/news/{{$latest[0]->slug}}"><img src="{{asset('assets/news/' .$latest[0]->img)}}" alt=""></a>
 						<div class="content"> <a href="">
-                            {{-- Tin mới nhất :{{$latest->title}} --}}
-                        <h2 class="section-under-title">{{$latest[0]->title}}</h2></a>
-                        <p>{{$latest[0]->summary}}</p>
+								{{-- Tin mới nhất :{{$latest->title}} --}}
+								<h2 class="section-under-title">{{$latest[0]->title}}</h2></a>
+							<p>{{$latest[0]->summary}}</p>
 						</div>
 					</div>
 				</div>
 				<div class="col-xl-6 col-md-6 col-12 wrapper">
 					<div class="article-wrapper">
 						<div class="date">
-                        <p>{{$latest[1]->datepost}}</p>
+							<p>{{$latest[1]->datepost}}</p>
 						</div>
-                    <div class="article-small"><a href="/news/{{$latest[1]->slug}}"><img src="{{asset('assets/news/'.$latest[1]->img)}}" alt=""></a>
+						<div class="article-small">
+							<div class="img">
+								<a href="/news/{{$latest[1]->slug}}">
+									<img src="{{asset('assets/news/'.$latest[1]->img)}}" alt="">
+								</a>
+							</div>
 							<div class="content"> <a href="">
-									<h2 class="section-under-title">{{$latest[1]->title}} </h2></a>
-                            <p>{{$latest[1]->summary}}</p>
+									<h2 class="section-under-title">{{$latest[1]->title}} </h2>
+								</a>
+								<p>{{$latest[1]->summary}}</p>
 							</div>
 						</div>
 					</div>
 					<div class="article-wrapper">
 						<div class="date">
-                        <p>{{$latest[2]->datepost}}</p>
+							<p>{{$latest[2]->datepost}}</p>
 						</div>
-                    <div class="article-small"><a href="/news/{{$latest[2]->slug}}"><img src="{{asset('assets/news/' .$latest[2]->img)}}" alt=""></a>
+						<div class="article-small">
+							<div class="img"><a href="/news/{{$latest[2]->slug}}"><img src="{{asset('assets/news/' .$latest[2]->img)}}" alt=""></a></div>
 							<div class="content"> <a href="">
-                            <h2 class="section-under-title">{{$latest[2]->title}}</h2></a>
-                            <p>{{$latest[2]->summary}}</p>
+									<h2 class="section-under-title">{{$latest[2]->title}}</h2>
+								</a>
+								<p>{{$latest[2]->summary}}</p>
 
 							</div>
 						</div>
@@ -58,33 +67,35 @@
 				</div>
 			</div>
 			<div class="row sec-2">
-                {{-- foreach --}}
-                @foreach ($news as $posts)
-                <div class="col-xl-4 col-md-4 col-sm-6 wrapper">
+				{{-- foreach --}}
+				@foreach ($news as $posts)
+				<div class="col-xl-4 col-md-4 col-sm-6 wrapper">
 					<div class="article-wrapper">
 						<div class="date">
-                        <p>{{$posts->datepost}}</p>
+							<p>{{$posts->datepost}}</p>
 						</div>
-						<?php $tomtat = \Illuminate\Support\Str::limit($posts->content,150,'...')
-            				?>
-                    <div class="article-small"><a href="/tin-tuc/{{$posts->slug}}"><img src="{{asset('assets/news/' .$posts->img)}}" alt=""></a>
-                    <div class="content"><a href="/tin-tuc/{{$posts->slug}}">
-                            <h2 class="section-under-title">{{$posts->title}}</h2></a>
-						<p><?php echo "".$tomtat;?></p>
-						</div>
+						<?php $tomtat = \Illuminate\Support\Str::limit($posts->content, 150, '...')
+						?>
+						<div class="article-small">
+							<div class="img"><a href="/tin-tuc/{{$posts->slug}}"><img src="{{asset('assets/news/' .$posts->img)}}" alt=""></a></div>
+							<div class="content"><a href="/tin-tuc/{{$posts->slug}}">
+									<h2 class="section-under-title">{{$posts->title}}</h2>
+								</a>
+								<p><?php echo "" . $tomtat; ?></p>
+							</div>
 
 
 						</div>
 					</div>
 				</div>
-                @endforeach
+				@endforeach
 			</div>
-            <div class="paginationSP mx-auto">
+			<div class="paginationSP mx-auto">
 				<div class="paginationSP-box mx-auto"><span class="material-icons button-s">skip_previous</span>
-                        <div class="paginationSP mx-auto">
-                            {{ $news->links() }}
-                            {{-- phân trang --}}
-                        </div>
+					<div class="paginationSP mx-auto">
+						{{ $news->links() }}
+						{{-- phân trang --}}
+					</div>
 				</div>
 			</div>
 		</div>
