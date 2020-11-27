c855589c6106c90b0812d2edf308fcd91a526682
QuangNguyen1607 <55746340+QuangNguyen1607@users.noreply.github.com>
2020-11-27 13:06:07 +0700

    nguyen

--- resources/views/admin/tintuc/danhsachtintuc.blade.php
+++ resources/views/admin/tintuc/danhsachtintuc.blade.php
@@ -1,62 +1,64 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Danh sách tin tức</title>
 </head>
+
 <body>
 
     {{-- @extends('layouts.master') --}}
     @extends('admin.sidebar')
     @section('content')
-    <div class="container">
-    <h2>Danh sách tin tức</h2>
+    <div class="container-fluid box-n-big">
+        <h2 class="section-title-big">Danh sách tin tức</h2>
 
-    <!-- table -->
-    <a name="" id="" class="btn btn-primary" href="{{route('news_deleteall')}}" role="button">Xoá tất cả tin tức</a>
-    {{-- <button type="submit" class="btn btn-primary" href="{{route('news_deleteall')}}">Xoá hết tin</button> --}}
-    <input class="form-control" id="myInput" type="text" placeholder="Search..">
-    <br>
-    <table class="table table-bordered table-striped">
-      <thead>
-        <tr>
-          <th>Mã</th>
-          <th>Tiêu đề</th>
-          <th>slug</th>
-          <th>Ngôn ngữ</th>
-          <th>Nội dung</th>
-          <th>summary</th>
-          <th>daypost</th>
-          <th>hình</th>
-          <th>tag</th>
-          <th>trạng thái</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody id="myTable">
-        @foreach($news ?? '' as $new)
-        <tr>
-          <td>{{$new->id}}</td>
-          <td>{{$new->title}}</td>
-          <td>{{$new->slug}}</td>
-          <td>{{$new->language}}</td>
-          <td>{{$new->content}}</td>
-          <td>{{$new->summary}}</td>
-          <td>{{$new->datepost}}</td>
-          <td><img class="img-tintuc" src="{{$new->img}}" alt=""></td>
-          <td>{{$new->tag}}</td>
-          <td>{{$new->status==1?'Đang hiện':'Đang ẩn'}}</td>
-          <td>
-            <a href="{{route('edit-new',$new->id)}}"> <button class="btn btn-info">Xem chi tiết</button> </a>
-            <a href="{{route('delete-new',$new->id)}}"> <button class="btn btn-danger">Xóa tin</button> </a>
-          </td>
-        </tr>
-        @endforeach
-
-      </tbody>
-    </table>
-    @endsection
+        <!-- table -->
+        <a name="" id="" class="btn btn-primary" href="{{route('news_deleteall')}}" role="button">Xoá tất cả tin tức</a>
+        <input class="form-control" id="myInput" type="text" placeholder="Tìm kiếm tin..">
+        <br>
+        <table class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th>Mã</th>
+                    <th>Tiêu đề</th>
+                    <th>slug</th>
+                    <th>Ngôn ngữ</th>
+                    <th>Nội dung</th>
+                    <th>summary</th>
+                    <th>daypost</th>
+                    <th>hình</th>
+                    <th>tag</th>
+                    <th>trạng thái</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody id="myTable">
+                @foreach($news ?? '' as $new)
+                <tr>
+                    <td>{{$new->id}}</td>
+                    <td>{{$new->title}}</td>
+                    <td>{{$new->slug}}</td>
+                    <td>{{$new->language}}</td>
+                    <td>{{$new->content}}</td>
+                    <td>{{$new->summary}}</td>
+                    <td>{{$new->datepost}}</td>
+                    <td><img class="img-tintuc" src="{{$new->img}}" alt=""></td>
+                    <td>{{$new->tag}}</td>
+                    <td>{{$new->status==1?'Đang hiện':'Đang ẩn'}}</td>
+                    <td>
+                        <a href="{{route('edit-new',$new->id)}}"> <button class="btn btn-info">Xem chi tiết</button>
+                        </a>
+                        <a href="{{route('delete-new',$new->id)}}"> <button class="btn btn-danger">Xóa tin</button> </a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        @endsection
 </body>
+
 </html>
