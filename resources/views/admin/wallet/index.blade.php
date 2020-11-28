a8c753b41b61e36e93e4463bf23821d66311781c
QuangNguyen1607 <55746340+QuangNguyen1607@users.noreply.github.com>
2020-11-28 13:59:22 +0700

    nguyen

--- resources/views/admin/wallet/index.blade.php
+++ resources/views/admin/wallet/index.blade.php
@@ -1,26 +1,28 @@
 @extends('admin.sidebar')
 @section('title','Nạp tiền')
 @section('content')
-<div class="card-body">
-        <div class="table-responsive">
-            <table class="table table-bordered" id="table" width="100%" cellspacing="0">
-            	<tbody>
-            		@foreach($users as $user)
-            		<tr>
-	            		<td>{{$user->username}}</td>
-	            		<td>{{number_format($user->wallet)}}</td>
-	            		<td>
-	            			<form action="{{route('add-wallet')}}" method="post">
-	            				@csrf
-	            				<input type="hidden" name="userid" value="{{$user->id}}">
-	            				<input type="number" name="wallet" value="">
-	            				<button type="submit">Thêm</button>
-	            			</form>
-	            		</td>
-            		</tr>
-            		@endforeach
-            	</tbody>
-            </table>
-        </div>
+<div class="container-fluid box-n-big page-nap-tien">
+    <div class="table-responsive">
+        <table class="table table-bordered" id="table" width="100%" cellspacing="0">
+            <tbody>
+                @foreach($users as $user)
+                <tr>
+                    <td>{{$user->username}}</td>
+                    <td>{{number_format($user->wallet)}}</td>
+                    <td>
+                        <form action="{{route('add-wallet')}}" method="post">
+                            @csrf
+                            <input type="hidden" name="userid" value="{{$user->id}}">
+                            <div class="wrap-button">
+                                <input type="number" name="wallet" value="">
+                                <button class="btn button-color" type="submit">Thêm</button>
+                            </div>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
+</div>
 @endsection
