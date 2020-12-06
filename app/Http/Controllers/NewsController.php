0cf1859728c74fd55100c93246845c71821a8b90
hoangtho <buinguyenhoangtho1997@gmail.com>
2020-12-06 14:27:35 +0700

    hoàn toàn xoá sạch tag rồi !

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use Str;
 
 
-use App\Models\tag;
 use App\Models\News;
 use App\Models\NewsCategory;
 use Carbon\Carbon;
@@ -89,9 +88,9 @@ class NewsController extends Controller
     {
         // lấy tất cả các danh mục trong tin tức
         $news_cate = NewsCategory::all();
-        $tag_input = tag::all();
 
-        return view('admin.tintuc.quanlytintuc',compact('news_cate','tag_input'));
+
+        return view('admin.tintuc.quanlytintuc',compact('news_cate'));
     }
     public function getNewsbyCate($slug)
     {
@@ -207,13 +206,12 @@ class NewsController extends Controller
     }
     public function getpostsbytag($tags){
 // news3 = get tất cả các tin theo tags
-        $news3 = DB::table('news')->where(
-            'tags','like','%'.$tags.'%')->paginate(3);
+
             $latest = DB::table('news')->orderBy('created_at','desc')->get();
             return view('pages/postsbytags')->with(
                 [
                     // 'tags'=>$tags,
-                    'news3'=>$news3,
+
                     'latest'=>$latest,
                 ]);
     }
