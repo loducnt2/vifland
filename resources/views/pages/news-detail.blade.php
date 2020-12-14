4f8764b1be7c4f0cf6d9081eebc9fadfc2b0f67b
Lâm Trần <lamzaaa@yahoo.com>
2020-12-14 14:11:57 +0700

    up

--- resources/views/pages/news-detail.blade.php
+++ resources/views/pages/news-detail.blade.php
@@ -21,7 +21,8 @@
 			<ol class="breadcrumb">
 				<li class="breadcrumb-item"><a href="#"> <i class="ri-arrow-left-line icons-breadcrum"></i>Mua/ Bán <span class="sll-breadcrum">&nbsp; (1.475.822 tin đăng)</span></a></li>
 				<li class="breadcrumb-item"><a href="#">
-						<p>Mở bán dự án đô thị sinh thái thông minh Aqua City, phía Đông thành phố Hồ Chí Minh Bạn tìm gì hôm nay?</p></a></li>
+						<p>Mở bán dự án đô thị sinh thái thông minh Aqua City, phía Đông thành phố Hồ Chí Minh Bạn tìm gì hôm nay?</p>
+					</a></li>
 			</ol>
 		</div>
 	</div>
@@ -32,21 +33,16 @@
 				<div class="col-xl-9 col-md-9">
 					<div class="article-container">
 						<div class="date">
-                        <p>{{$news->datepost}}</p>
-                        </div>
-                        <br>
-                        <a href="/tin-tuc/danh-muc/{{$news_cate->slug}}">{{$news_cate->category_name}}</a>
-                        <hr class="my-4">
+							<p>{{$news->datepost}}</p>
+						</div>
 						<div class="title">
-
-        			<h1 class="section-under-title">{{$news->title}}</h1>
-                        </div>
-
-                    <p>Tác giả: <a href="/user/profile/{{$id_nguoidang->id}}">{{$id_nguoidang->username}} <span class="badge badge-primary">Quản trị viên</span></h6></a></p>
+							<h1 class="section-under-title">{{$news->title}}</h1>
+						</div>
+						<p>Tác giả: <a href="/user/profile/{{$id_nguoidang->id}}">{{$id_nguoidang->username}} <span class="badge badge-primary">Quản trị viên</span></h6></a></p>
+						<a href="/tin-tuc/danh-muc/{{$news_cate->slug}}">{{$news_cate->category_name}}</a>
 						<div class="content">
-                        {!!$news->content!!}
-
-                        </div>
+							{!!$news->content!!}
+						</div>
 					</div>
 				</div>
 				<div class="col-xl-3 col-md-3">
@@ -54,23 +50,24 @@
 						<div class="title">
 							<h2 class="section-des">Các tin khác</h2>
 						</div>
-                        @foreach ($posts as $news2)
-                        @if($news->slug == $news2->slug)
-                        {{-- ẩn tin --}}
-                        @else
-                            <div class="news-content">
+						@foreach ($posts as $news2)
+						@if($news->slug == $news2->slug)
+						{{-- ẩn tin --}}
+						@else
+						<div class="news-content">
 
-                                <div class="img"> <img src="{{asset('assets/news')}}/{{$news2->img}}" alt=""></div>
-                                <div class="content">
-                                    <div class="date">
-                                    <p>{{$news2->datepost}}</p>
-                                    </div><a href="/news/{{$news2->slug}}">
-                                    <h2 class="section-des">{{$news2->title}}</h2></a>
+							<div class="img"> <img src="{{asset('assets/news')}}/{{$news2->img}}" alt=""></div>
+							<div class="content">
+								<div class="date">
+									<p>{{$news2->datepost}}</p>
+								</div><a href="/news/{{$news2->slug}}">
+									<h2 class="section-des">{{$news2->title}}</h2>
+								</a>
 
-                                </div>
-                            </div>
-                        @endif
-                        @endforeach
+							</div>
+						</div>
+						@endif
+						@endforeach
 					</div>
 				</div>
 			</div>
