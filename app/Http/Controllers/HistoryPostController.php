827d423b0ee5d332c34f95a7756f102be9872d23
QuangNguyen1607 <55746340+QuangNguyen1607@users.noreply.github.com>
2020-12-20 12:53:27 +0700

    nguyen

--- app/Http/Controllers/HistoryPostController.php
+++ app/Http/Controllers/HistoryPostController.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use App\Models\PostHistory;
 use App\Models\Product;
@@ -9,6 +11,7 @@ use App\Models\ProductImg;
 use App\Models\Favorited;
 use Illuminate\Support\Facades\DB;
 use App\User;
+
 class HistoryPostController extends Controller
 {
     /**
@@ -18,18 +21,18 @@ class HistoryPostController extends Controller
      */
     public function index()
     {
-       $news = PostHistory::leftJoin('product','post_history.product_id','product.id')
-       ->orderby('post_history.status', 'asc')
-       ->orderby('product.type', 'asc')
-        ->select(
-            'product.title as product_title' ,
-            'post_history.status as status',
-            'post_history.id as post_id',
-            'product.id as product_id',
-            'product.type as product_type',
-        )
-        ->paginate(10);
-        return view('/admin/tintuc/danhsachduyettin',compact('news'));
+        $news = PostHistory::leftJoin('product', 'post_history.product_id', 'product.id')
+            ->orderby('post_history.status', 'asc')
+            ->orderby('product.type', 'asc')
+            ->select(
+                'product.title as product_title',
+                'post_history.status as status',
+                'post_history.id as post_id',
+                'product.id as product_id',
+                'product.type as product_type',
+            )
+            ->paginate(10);
+        return view('/admin/tintuc/danhsachduyettin', compact('news'));
     }
     /**
      * Show the form for creating a new resource.
@@ -58,34 +61,34 @@ class HistoryPostController extends Controller
      */
     public function show($id)
     {
-        $product = Product::where('product.id',$id)
-        ->leftJoin('product_extend','product.id','product_extend.product_id')
-        ->leftJoin('product_unit','product_extend.unit_id','product_unit.id')
-        ->leftJoin('province','product.province_id','province.id')
-        ->leftJoin('district','product.district_id','district.id')
-        ->leftJoin('ward','product.ward_id','ward.id')
-        ->select(
-            'product_extend.*',
-            'product.*',
-            'product_extend.id as productex_id',
-            'province.name as province',
-            'district.name as district',
-            'ward.name as ward',
-            'product_unit.name as unit'
-        )
-        ->first();
-        $product_cate = TypeProduct::where('product_extend_id',$product->productex_id)
-        ->leftJoin('product_cate','type_of_product.product_cate_id','product_cate.id')->get();
-       $acreage = doubleval( $product->depth*$product->facades );
-       $total   = intval($product->price)*$acreage;
-       $product->update(['view'=> $product->view + 1 ]);
-       $cate    = Category::where('id',$product->cate_id)->value('name');
-       $image     = ProductImg::where('product_extend_id',$product->productex_id)->select('name')->get();
-       $new = PostHistory:: where('product_id',$id)
-       ->first();
+        $product = Product::where('product.id', $id)
+            ->leftJoin('product_extend', 'product.id', 'product_extend.product_id')
+            ->leftJoin('product_unit', 'product_extend.unit_id', 'product_unit.id')
+            ->leftJoin('province', 'product.province_id', 'province.id')
+            ->leftJoin('district', 'product.district_id', 'district.id')
+            ->leftJoin('ward', 'product.ward_id', 'ward.id')
+            ->select(
+                'product_extend.*',
+                'product.*',
+                'product_extend.id as productex_id',
+                'province.name as province',
+                'district.name as district',
+                'ward.name as ward',
+                'product_unit.name as unit'
+            )
+            ->first();
+        $product_cate = TypeProduct::where('product_extend_id', $product->productex_id)
+            ->leftJoin('product_cate', 'type_of_product.product_cate_id', 'product_cate.id')->get();
+        $acreage = doubleval($product->depth * $product->facades);
+        $total   = intval($product->price) * $acreage;
+        $product->update(['view' => $product->view + 1]);
+        $cate    = Category::where('id', $product->cate_id)->value('name');
+        $image     = ProductImg::where('product_extend_id', $product->productex_id)->select('name')->get();
+        $new = PostHistory::where('product_id', $id)
+            ->first();
 
-        return view('/admin/tintuc/chitietduyettin',compact('product','acreage','total','product_cate','cate','image','new'));
-    //return view('pages/article/article',compact('product','acreage','total','product_cate','cate','image'));
+        return view('/admin/tintuc/chitietduyettin', compact('product', 'acreage', 'total', 'product_cate', 'cate', 'image', 'new'));
+        //return view('pages/article/article',compact('product','acreage','total','product_cate','cate','image'));
     }
     /**
      * Show the form for editing the specified resource.
@@ -104,14 +107,15 @@ class HistoryPostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update( $id)
+    public function update($id)
     {
-        $post = PostHistory::where('product_id',$id)
-        ->update(
-            [
-            "status" => 1
-        ]);
-    return redirect('/admin/danh-sach-duyet-tin');
+        $post = PostHistory::where('product_id', $id)
+            ->update(
+                [
+                    "status" => 1
+                ]
+            );
+        return redirect('/admin/danh-sach-duyet-tin');
     }
     /**
      * Remove the specified resource from storage.
@@ -122,37 +126,39 @@ class HistoryPostController extends Controller
     public function destroy($id)
     {
         $pro = Product::find($id);
-        $favor= Favorited:: where('product_extend_id',$id);
+        $favor = Favorited::where('product_id', $id);
         $pro->delete();
         $favor->delete();
-        
+
         return redirect('/admin/danh-sach-duyet-tin');
     }
-    public function updatePost($id){
+    public function updatePost($id)
+    {
         $post = PostHistory::find($id);
         $post->update([
             'status' => 1
         ]);
-       return redirect('/admin/danh-sach-duyet-tin');
+        return redirect('/admin/danh-sach-duyet-tin');
     }
-    public function cancelPost($id){
+    public function cancelPost($id)
+    {
         //ví tiền của user
-        $wallet = PostHistory::where('post_history.product_id',$id)
-        ->leftJoin('user','post_history.user_id','user.id')
-        ->value('user.wallet');
+        $wallet = PostHistory::where('post_history.product_id', $id)
+            ->leftJoin('user', 'post_history.user_id', 'user.id')
+            ->value('user.wallet');
         //giá tiền đăng bài
-        $price_post = Product::where('product.id',$id)
-        ->value('product.price_post');
-        $product = Product::where('product.id',$id)
-        ->leftJoin('post_history','product.id','post_history.product_id')
-        ->leftJoin('user','post_history.user_id','user.id')
-        ->update([
-            'product.soft_delete' => 1,
-            'product.datetime_end'=> date('Y-m-d H:i:s',strtotime('now')),
-            'product.datetime_delete'=>date('Y-m-d H:i',strtotime('now'.' '.'+'.' '. 7 .' '.'days') ),
-            'post_history.status' => 2,
-            'user.wallet'         =>  doubleval($wallet + $price_post),
-        ]);
+        $price_post = Product::where('product.id', $id)
+            ->value('product.price_post');
+        $product = Product::where('product.id', $id)
+            ->leftJoin('post_history', 'product.id', 'post_history.product_id')
+            ->leftJoin('user', 'post_history.user_id', 'user.id')
+            ->update([
+                'product.soft_delete' => 1,
+                'product.datetime_end' => date('Y-m-d H:i:s', strtotime('now')),
+                'product.datetime_delete' => date('Y-m-d H:i', strtotime('now' . ' ' . '+' . ' ' . 7 . ' ' . 'days')),
+                'post_history.status' => 2,
+                'user.wallet'         =>  doubleval($wallet + $price_post),
+            ]);
         return  redirect()->back();
     }
 }
--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -4,6 +4,11 @@
 <!-- Thêm styles cho trang này ở đây-->
 @stop
 @section('content')
+<style>
+    label.error {
+        position: static !important;
+    }
+</style>
 <main>
     <section class="banner-top">
         <div class="img"> </div>
@@ -18,7 +23,7 @@
                     </a>
                 </li>
                 <li class="breadcrumb-item"><a href="{{route('cate1')}}">
-                        <p>Liên hệ</p>
+                        <p>Liên hệ 123</p>
                     </a></li>
             </ol>
         </div>
@@ -30,8 +35,7 @@
                 <div class="row">
                     <div class="col-xl-5">
                         <div class="warpper-content">
-                            <div class="logo"> <img class="lazyload" data-src="./assets/logo/logo-footer-300.png"
-                                    alt=""></div>
+                            <div class="logo"> <img class="lazyload" data-src="./assets/logo/logo-footer-300.png" alt=""></div>
                             <h2 class="section-under-title">CÔNG TY CỔ PHẨN BẤT ĐỘNG SẢN VIVA HOMES</h2>
                             <div class="info">
                                 <div class="wrapper"><span class="material-icons">room</span>
@@ -81,75 +85,73 @@
                                         <button class="btn btn-submit" type="submit">Gửi</button>
                                     </div>
                                 </form>
-                                <script src="https://code.jquery.com/jquery-3.5.1.min.js"
-                                    integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-                                    crossorigin="anonymous"></script>
+                                <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
                                 <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.2/dist/jquery.validate.js
 "></script>
                                 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js">
                                 </script>
                                 <script>
-                                $(document).ready(function() {
-                                    // $("#form-contact1").hide();
-                                    $("#form-contact1").validate({
-                                        onfocusout: false,
-                                        onkeyup: false,
-                                        onclick: false,
-                                        rules: {
-                                            "name": {
-                                                required: true,
+                                    $(document).ready(function() {
+                                        // $("#form-contact1").hide();
+                                        $("#form-contact1").validate({
+                                            onfocusout: false,
+                                            onkeyup: false,
+                                            onclick: false,
+                                            rules: {
+                                                "name": {
+                                                    required: true,
+
+                                                },
+                                                "email": {
+                                                    required: true,
+                                                    email: true
+                                                },
+                                                "title": {
+                                                    required: true,
+                                                },
+                                                "address": {
+                                                    required: true,
+                                                },
+                                                "phone": {
+                                                    required: true,
+                                                    number: true,
+                                                    minlength: 10
+                                                },
+                                                "content": {
+                                                    required: true,
+                                                },
 
                                             },
-                                            "email": {
-                                                required: true,
-                                                email: true
-                                            },
-                                            "title": {
-                                                required: true,
-                                            },
-                                            "address": {
-                                                required: true,
-                                            },
-                                            "phone": {
-                                                required: true,
-                                                number: true,
-                                                minlength: 10
-                                            },
-                                            "content": {
-                                                required: true,
-                                            },
+                                            messages: {
+                                                "name": {
+                                                    required: "Bắt buộc nhập tên",
 
-                                        },
-                                        messages: {
-                                            "name": {
-                                                required: "Bắt buộc nhập tên",
+                                                },
+                                                "email": {
+                                                    required: "Bắt buộc nhập Email",
+                                                    email: "Vui lòng nhập đúng email"
+                                                },
+                                                "phone": {
+                                                    required: "Bắt buộc nhập số điện thoại",
+                                                    number: "Vui lòng nhập đúng số điện thoại",
+                                                    minlength: "Vui lòng nhập đúng số điện thoại",
+                                                },
+                                                "address": {
+                                                    required: "Bắt buộc nhập địa chỉ",
+                                                },
+                                                "title": {
+                                                    required: "Bắt buộc nhập tiêu đề",
 
-                                            },
-                                            "email": {
-                                                required: "Bắt buộc nhập Email",
-                                                email: "Vui lòng nhập đúng email"
-                                            },
-                                            "phone": {
-                                                required: "Bắt buộc nhập số điện thoại",
-                                                number: "Vui lòng nhập đúng số điện thoại",
-                                                minlength: "Vui lòng nhập đúng số điện thoại",
-                                            },
-                                            "address": {
-                                                required: "Bắt buộc nhập địa chỉ",
-                                            },
-                                            "title": {
-                                                required: "Bắt buộc nhập tiêu đề",
+                                                },
+                                                "content": {
+                                                    required: "Bắt buộc nhập nội dung",
 
-                                            },
-                                            "content": {
-                                                required: "Bắt buộc nhập nội dung",
-
-                                            },
+                                                },
 
 
-                                        }
-                                    });
-                                })
+                                            }
+                                        });
+                                    })
                                 </script>
 
 
@@ -162,10 +164,7 @@
         <section class="lienhe-l-2">
             <div class="container">
                 <div class="map">
-                    <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1333.0493696577748!2d106.62953699383226!3d10.852832765862265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317529e76ef4ac4d%3A0x30d6a9932e802efe!2sFPT%20Polytechnic%20HCM%20-%20C%C6%A1%20s%E1%BB%9F%203!5e0!3m2!1svi!2s!4v1601302166398!5m2!1svi!2s"
-                        width="100%" height="500" frameborder="0" style="border:0;" allowfullscreen=""
-                        aria-hidden="false" tabindex="0"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1333.0493696577748!2d106.62953699383226!3d10.852832765862265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317529e76ef4ac4d%3A0x30d6a9932e802efe!2sFPT%20Polytechnic%20HCM%20-%20C%C6%A1%20s%E1%BB%9F%203!5e0!3m2!1svi!2s!4v1601302166398!5m2!1svi!2s" width="100%" height="500" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
                 </div>
             </div>
         </section>
